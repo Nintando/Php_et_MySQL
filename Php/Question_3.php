3ddfeb2a87b04fdbfba8d9abed0014dc1cc17a69
Nintando <nintandot@gmail.com>
2022-02-16 15:40:12 +0100

    php

--- Php/Question_3.php
+++ Php/Question_3.php
@@ -14,10 +14,22 @@
         
     }
 
+    function AttaqueCoupDeHache(){
+        $class = new Guerrier (1,"Pierre","20",100,"hache");
+        $dmg = $class ->getForce()*1.5;
+        $rate = rand(1,2);
+        if ($rate===1){
+            echo 'attaque raté';
+        }
+        else{
+            echo 'attaque réussi : '.$dmg;
+        }
+    }
 
     
 ?>
 
+<button onclick=<?php AttaqueCoupDeHache() ?>>Attaque</button>
 <form method='post'>
     <input name='lvlUp' type='submit' value='LvlUp'>
     <h1><?php echo $_SESSION['lvl']++ ?></h1>
